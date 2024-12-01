fd105870345c545f30204529e5f1beffdf7d8d02
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-12-01 16:57:42 +0500

    Experemintal build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,30 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+echo "Done"
+KERNEL_DIR=$(pwd)
+PATH="${PWD}/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=Eblan
+export KBUILD_BUILD_USER="Pizda"
+# Compile plox
+function compile() {
 
-DIR=$(pwd)
-OUT_DIR=$DIR/out
-
-if [ ! -d $DIR/prebuilts/clang-r416183b ]; then
-    echo 'clone Clang Prebuilt'
-    git clone https://github.com/Klozz/Yuki-clang $DIR/prebuilts/clang
-fi
-
-if [ ! -d $DIR/prebuilts/gcc ]; then
-    echo 'clone prebuilt gcc arm'
-    git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git $DIR/prebuilts/gcc
-fi
-
-if [ ! -d $DIR/prebuilts/gcc64 ]; then
- echo 'clone prebuilt gcc aarch64'
-    git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git $DIR/prebuilts/gcc64
-fi
-
-export PATH=$DIR/prebuilts/clang/bin:$PATH
-export PATH=$DIR/prebuilts/gcc/bin:$PATH
-export PATH=$DIR/prebuilts/gcc64/bin:$PATH
-
-CLANG="$DIR/prebuilts/clang/bin"
-GCC="$DIR/prebuilts/gcc/bin"
-GCC64="$DIR/prebuilts/gcc64/bin"
-
-PATH="$CLANG:$GCC64:$GCC:$PATH" make O=out ARCH=arm64 a71_defconfig
-PATH="$CLANG:$GCC64:$GCC:$PATH" make -j8 O=$OUT_DIR ARCH=arm64 CC=clang LTO=thin CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androidebi-
+    make O=out ARCH=arm64 a71_defconfig
+    make -j$(nproc --all) O=out \
+                          ARCH=arm64 \
+			  LTO=thin \
+			  CC=clang \
+			  CROSS_COMPILE=aarch64-linux-gnu- \
+			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+compile
